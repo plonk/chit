f871c3f1f95639525feeffd7c7247cc5b55fc544
Ruth <luca.syureria-12.21.26@hotmail.co.jp>
2019-07-07 20:33:31 +0900

    typo in post_message_shitaraba()
    したらばの板番号の変数名が間違っていたので修正
    PostのBBSで指定するとこは調べたけどわからなかったので、雰囲気で修正

--- post-test.rb
+++ post-test.rb
@@ -84,15 +84,15 @@ end
 
 def post_message_shitaraba(board, thread, name, mail, body)
   cat, board_num = board.category, board.board_num
-  uri = URI("https://jbbs.shitaraba.net/bbs/write.cgi/#{cat}/#{board}/#{thread}/")
-  referer = "https://jbbs.shitaraba.net/bbs/read.cgi/#{cat}/#{board}/#{thread}/"
+  uri = URI("https://jbbs.shitaraba.net/bbs/write.cgi/#{cat}/#{board_num}/#{thread}/")
+  referer = "https://jbbs.shitaraba.net/bbs/read.cgi/#{cat}/#{board_num}/#{thread}/"
 
   Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
     req = Net::HTTP::Post.new(uri)
     req['User-Agent'] = "Chit/0.0.1"
     req['Referer'] = referer
     req.set_form_data(
-      "BBS" => board.to_s,
+      "BBS" => board_num.to_s,
       "KEY" => thread.to_s,
       "DIR" => cat,
       "NAME"=> to_euc(name),
