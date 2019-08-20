b5ce5d711fc397ee6cc1ad3ab9b33d38aaea8086
Ruth <luca.syureria-12.21.26@hotmail.co.jp>
2019-08-20 23:32:31 +0900

    書き込み時クッキー作成送信
    5chやぜろちゃんねるプラス等、書き込み時クッキー確認画面により書き込めない問題を解決

--- post-test.rb
+++ post-test.rb
@@ -56,6 +56,11 @@ def post_message_nichan(board, thread, name, mail, body)
       "key"=>thread.to_s,
       "submit"=>to_sjis("書き込む"),
     )
+    unless $cookie.nil?
+      req.add_field('Cookie', $cookie.map{|k, v|
+          "#{k}=#{v}"
+      }.join(';'))
+    end
 
     res = http.request(req)
 
@@ -63,7 +68,28 @@ def post_message_nichan(board, thread, name, mail, body)
     when Net::HTTPOK
       res_body = sjis_to_utf8(res.body)
       unless res_body =~ /書きこみました/
-        STDERR.puts "Warning: サーバーのOK応答に成功メッセージが確認できませんでした。"
+        #クッキー確認により書き込み失敗　gsubでまとめたい
+        if res_body =~ /<title>(.+)<\/title>/i
+          STDERR.puts "Warnning: " + $1
+          if res_body =~ /<div.*>\n投稿確認(.+)<\/div>/m
+            puts "書きこみ＆クッキー確認\n投稿確認"
+            puts $1.gsub(/<br>/i, "")
+            puts "上記を承諾して書き込む yes/no?"
+            if STDIN.gets =~ /y/i
+              cookie = {}
+              res.get_fields('Set-Cookie').each{|str|
+                k,v = str[0...str.index(';')].split('=')
+                cookie[k] = v
+              }
+              $cookie = cookie
+              post_message_nichan(board, thread, name, mail, body)
+            end
+
+          end
+        else
+          STDERR.puts "Warning: サーバーのOK応答に成功メッセージが確認できませんでした。"
+          #STDERR.puts res_body
+        end
       end
       return true
     else
